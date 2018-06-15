51ba860ebd41ddb1199a8488cf2d71e5e919bc35
DiLeyRa <ing.dianaleyva16@gmail.com>
2018-06-15 13:09:15 -0500

    Cambios 2

---
+++ src/README.md
@@ -0,0 +1,8 @@
+Definición del usuario
+
+Se pretende crear una interfaz amigable y facil de manipular en la que un usuario pueda mandar mensajes cifrados a sus amigos, familiares, conocidos etc; 
+
+
+Usabilidad e introduccion a la aplicación
+
+La aplicación Cifrado Cesar consiste en que una letra del texto original es reemplazada por otra letra que se encuentra un numero de posciones delante en el abecedario, con ayuda de una clave, la cual es bien el numero de lugares que se desplazara la letra, se pretende que el problema se resuelva con la creacion de una interfaz en donde el usuario pueda ingresar un texto, las posiciones que el cifrado desplace cada caracter y al hacer click en el boton de cifrado, se imprima en la pantalla el texto ya encriptado, de igual manera al introducir el texto cifrado y el numero de caracteres a recorrer nos imprimira el mensaje descifrado.
--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,80 @@
 window.cipher = {
-  // ...
+encode : (offset, string) =>{//una funcion dentro de un objeto se llama metodo
+    let newMessage = [];
+    let encryptMessage = [];
+    let encryptMessageString;
+
+    for (let i=0; i<string.length; i++){
+      console.log(string.charAt(i));
+      if(string.charCodeAt(i)=== 32){//Para los espacios en blanco
+        encryptMessage[i]= " ";
+      }else{//para las demas letras
+        if(string.charCodeAt(i)<=90 && string.charCodeAt(i)>=65){
+          //cifrado mayusculas
+            newMessage[i] = (((string.charCodeAt(i)) - 65 + offset) % 26 + 65);
+            console.log("***");
+            console.log(newMessage[i]);
+            console.log("***");
+            encryptMessage[i]= String.fromCharCode(newMessage [i]);
+          }else if (string.charCodeAt(i)<=122 && string.charCodeAt(i)>=97){
+            //cifrado minusculas
+              newMessage[i]= (((string.charCodeAt(i)) - 97 + offset) % 26 + 97);
+              console.log(newMessage);
+              encryptMessage[i]= String.fromCharCode(newMessage [i]);
+          }
+      }
+    }
+    encryptMessageString = encryptMessage.join("");
+    return encryptMessageString;
+  },
+  decode : (offset, string) =>{
+    let newMessage = [];
+    let encryptMessage = [];
+    let encryptMessageString;
+
+
+    for (let i = 0; i<string.length; i++){
+      if(string.charCodeAt(i)=== 32){//Para los espacios en blanco
+        encryptMessage [i]= " ";
+      }else{//para las demas letras
+
+        if(string.charCodeAt(i)<=90 && string.charCodeAt(i)>=65){
+          //descifrado mayusculas
+          if (((string.charCodeAt(i)) - 65 - offset)<0){
+            newMessage [i]=   (((string.charCodeAt(i)) - 65 - offset) % 26 + 91);
+            console.log("***");
+            console.log(newMessage[i]);
+            console.log("***");
+            encryptMessage [i]= String.fromCharCode(newMessage [i]);
+            //console.log(encryptMessage);
+          }
+            else{newMessage [i]=   (((string.charCodeAt(i)) - 65 - offset) % 26 + 65);
+              console.log(newMessage);
+            encryptMessage [i]= String.fromCharCode(newMessage [i]);
+          }
+
+          }else if (string.charCodeAt(i)<=122 && string.charCodeAt(i)>=97){
+            //descifrado minusculas
+            if(((string.charCodeAt(i)) - 97 - offset)<0){
+              newMessage [i]= (((string.charCodeAt(i)) - 97 - offset) % 26 + 123);
+              console.log("***");
+              console.log(newMessage[i]);
+              console.log("***");
+              encryptMessage [i]= String.fromCharCode(newMessage [i]);
+            }else{
+              newMessage [i]= (((string.charCodeAt(i)) - 97 - offset) % 26 + 97);
+                console.log(newMessage);
+              encryptMessage [i]= String.fromCharCode(newMessage [i]);
+            }
+
+          }
+      }
+
+    }
+console.log(encryptMessage);
+    encryptMessageString = encryptMessage.join("");
+    return encryptMessageString;
+
+  }
+
 };
